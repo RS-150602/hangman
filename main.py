5533a0daae7712639745be795e3a6fe5295edbe8
Rayan_150602 <105919183+RS-150602@users.noreply.github.com>
2024-01-10 14:55:32 +0000

    main.py

--- main.py
+++ main.py
@@ -1,52 +1,80 @@
-import os
-import random 
-from word_list import word_list
-from art import stages
-
-print("Welcome to hangman!!!")
-rand_word = random.choice(word_list)
-word_length = int(len(rand_word))
+def main():   
+    import os
+    import random 
+    from word_list import word_list
+    from art import stages
 
 
-display = []
-for _ in range(word_length):
-    display += "_"
-lives = 6
+
+    print("Welcome to hangman!!!")
+    rand_word = random.choice(word_list)
+    word_length = int(len(rand_word))
 
 
-while True:
-    user_guess = input("Guess the word: ").lower()
 
-    if user_guess in display:
-        input("You have already guessed this letter.")
+    display = []
+    for _ in range(word_length):
+        display += "_"
+    lives = 6
 
-    for position in range(0,word_length):
-        letter = rand_word[position]
-        if letter == user_guess:
-            display[position] = letter
-        
-    
 
-    if user_guess not in rand_word:
-        input(f"you guessed {user_guess}, thats not in the word, you lose a life.")
 
-        lives -= 1
-        if lives == 0:
-            print("You lose!!!")
-            print(f"The word was {rand_word}")
-            input("")
-            break
+    while True:
+        user_guess = input("Guess the word: ").lower()
+
+        if user_guess in display:
+            input("You have already guessed this letter.")
+
+        for position in range(0,word_length):
+            letter = rand_word[position]
+            if letter == user_guess:
+                display[position] = letter
+            
         
 
-    if user_guess in display:
-      print("You have already chosen this letter.")
+        if user_guess not in rand_word:
+            input(f"You guessed {user_guess}, that's incorrect. -1 life.")
+            lives -= 1
+            if lives == 0:
+                os.system('cls')
+                print("You lose!!!")
+                print(f"The word was {rand_word}")
+                while True:
+                    ans1 = input("Would you like to try again? y/n: ")
+                    if ans1 == "y":
+                        os.system("cls")
+                        main()
+                    elif ans1 =="n":
+                        os.system('cls')
+                        exit()
+                    else:
+                        print("Invalid Input, Please try again.")
+                        continue
 
-    os.system('cls')
-    print(stages[lives])
-    print(' '.join(display))
-    
 
-    if "_" not in display:
-        print("You win!!!")
-        input("")
-        break
+                
+        if user_guess in display:
+            print("You have already chosen this letter.")
+
+
+
+        os.system('cls')
+        print(stages[lives])
+        print(' '.join(display))
+        
+
+
+        if "_" not in display:
+            print("You win!!!")
+            while True:
+                ans1 = input("Would you like to try again? y/n: ")
+                if ans1 == "y":
+                    os.system("cls")
+                    main()
+                elif ans1 =="n":
+                    os.system('cls')
+                    exit()
+                else:
+                    print("Invalid Input, Please try again.")
+                    continue
+main()
